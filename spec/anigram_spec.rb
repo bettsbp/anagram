0963b5dd06db04b9ff703e95485e6fbc6918908a
Brandon Betts <bettsb.brandon@gmail.com>
2018-06-29 11:13:15 -0700

    secondary test works for first test

--- spec/anigram_spec.rb
+++ spec/anigram_spec.rb
@@ -6,6 +6,12 @@ describe("Anigram#check_if_anigram") do
   it("will see if ruby is an anagram of bury and return true") do
     words_to_test = Words.new("ruby","bury")
   end
+  it("will see if any word is an anagram of some other word and return true") do
+    words_to_test = Words.new("binary","brainy")
+  end
+  it("will see if ruby is an anagram of bury and return true even though there is capitalization") do
+    words_to_test = Words.new("rUBy","Bury")
+  end
   it("will see if ruby is an anagram of bury and return true even though there is capitalization") do
     words_to_test = Words.new("rUBy","Bury")
   end
