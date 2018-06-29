a06661d0779815af2c5e57ecb29507d08b687816
Brandon Betts <bettsb.brandon@gmail.com>
2018-06-29 13:59:03 -0700

    add FUNCTION CODE YAY

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -6,7 +6,7 @@ class Words
     # this is what im using currently
     @word_array_1 = @input_word_1.split("").sort
     @word_array_2 = @input_word_2.split("").sort
-    @vowels = ["a", "e", "i", "o", ""]
+    @vowels = ["a", "e", "i", "o", "u"]
   end
 
   # first method
@@ -24,13 +24,13 @@ class Words
     end
   end
 
-  def check_for_vowels
-    if ((@word_array_1 & @vowels).any? == true && (@word_array_2 & @vowels).any? == true)
-      check_if_anagram
-    else
-    # elsif ((@word_array_1 & @vowels).any? == false && (@word_array_2 & @vowels).any? == false)
-      "Please enter real words"
-    end
-  end
+  # def check_for_vowels
+  #   if ((@word_array_1 & @vowels).any? == true && (@word_array_2 & @vowels).any? == true)
+  #     check_if_anagram
+  #   else
+  #   # elsif ((@word_array_1 & @vowels).any? == false && (@word_array_2 & @vowels).any? == false)
+  #     "Please enter real words"
+  #   end
+  # end
 
 end
