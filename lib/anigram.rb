874fc7d90f556429b84849ec2cd45eba3a9aa953
Brandon Betts <bettsb.brandon@gmail.com>
2018-06-29 09:45:41 -0700

    add first passing test

--- lib/anigram.rb
+++ lib/anigram.rb
@@ -1,13 +1,16 @@
-class Word
+class Words
   def initialize(input_word_1, input_word_2)
     @input_word_1 = input_word_1
     @input_word_2 = input_word_2
-    @word_array_1 = @input_word_1.split("")
-    @word_array_2 = @input_word_2.split("")
+    @word_array_1 = @input_word_1.split("").sort_by(&:downcase)
+    @word_array_2 = @input_word_2.split("").sort_by(&:downcase)
   end
 
   def check_if_anigram
-    
-
+    if @word_array_1 == @word_array_2
+      true
+    else
+      false
+    end
   end
 end
---
+++ spec/anigram_spec.rb
@@ -0,0 +1,9 @@
+require("rspec")
+require("pry")
+require("anigram")
+
+describe("Anigram#check_if_anigram") do
+  it("will see if ruby is an anagram of bury and return true") do
+    words_to_test = Words.new("ruby","bury")
+  end
+end
