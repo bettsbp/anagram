545978bdc2e722cd11e4c325a2707fbfc8e867ac
Brandon Betts <bettsb.brandon@gmail.com>
2018-06-29 09:50:57 -0700

    second passing test

--- lib/anigram.rb
+++ lib/anigram.rb
@@ -1,11 +1,16 @@
 class Words
   def initialize(input_word_1, input_word_2)
+    # these are here if i need to access the individual words later on
     @input_word_1 = input_word_1
     @input_word_2 = input_word_2
+    # this is what im using currently
     @word_array_1 = @input_word_1.split("").sort_by(&:downcase)
     @word_array_2 = @input_word_2.split("").sort_by(&:downcase)
+    # vowels list
+    @vowels = ["a", "e", "i", "o", "u"]
   end
 
+  # first method
   def check_if_anigram
     if @word_array_1 == @word_array_2
       true
--- spec/anigram_spec.rb
+++ spec/anigram_spec.rb
@@ -6,4 +6,7 @@ describe("Anigram#check_if_anigram") do
   it("will see if ruby is an anagram of bury and return true") do
     words_to_test = Words.new("ruby","bury")
   end
+  it("will see if ruby is an anagram of bury and return true even though there is capitalization") do
+    words_to_test = Words.new("rUBy","Bury")
+  end
 end
