40c7dd1d62a9b66da21c319181d37ad98f023b0c
Brandon Betts <bettsb.brandon@gmail.com>
2018-06-29 09:14:36 -0700

    add gemfile, add beginnings to first method

--- lib/anigram.rb
+++ lib/anigram.rb
@@ -1,9 +1,13 @@
 class Word
-  def initialize(input_word)
-    @input_word = input_word
+  def initialize(input_word_1, input_word_2)
+    @input_word_1 = input_word_1
+    @input_word_2 = input_word_2
+    @word_array_1 = @input_word_1.split("")
+    @word_array_2 = @input_word_2.split("")
   end
 
   def check_if_anigram
     
+
   end
 end
